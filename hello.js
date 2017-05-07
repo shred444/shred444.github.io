d51f5e25e9e406a16f50f610dccc74767cb88419
Jonathan Cohn <shred444@gmail.com>
2017-05-07 19:35:39 -0400

    changed url

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "api.darksky.net/forecast/2ba7132f09fc2069522d9a980d4b741b/42.4851,-71.4328"
+        url: "https://api.darksky.net/forecast/2ba7132f09fc2069522d9a980d4b741b/42.4851,-71.4328"
     }).then(function(data) {
        $('.greeting-id').append(data.currently);
        $('.greeting-content').append(data.content);
