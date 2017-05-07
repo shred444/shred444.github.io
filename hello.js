4d7cedb8b0764c99432da96817a0d81908c1e2f5
Jonathan Cohn <shred444@gmail.com>
2017-05-07 19:15:25 -0400

    forecast.io

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://rest-service.guides.spring.io/greeting"
+        url: "api.darksky.net/forecast/2ba7132f09fc2069522d9a980d4b741b/42.4851,-71.4328"
     }).then(function(data) {
-       $('.greeting-id').append(data.id);
+       $('.greeting-id').append(data.currently);
        $('.greeting-content').append(data.content);
     });
 });
