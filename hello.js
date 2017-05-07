575279e741e48affa8589cf727f4e1a55717e869
Jonathan Cohn <shred444@gmail.com>
2017-05-07 19:55:06 -0400

    d

--- hello.js
+++ hello.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
     $.ajax({
-        url: "https://api.darksky.net/forecast/2ba7132f09fc2069522d9a980d4b741b/42.4851,-71.4328"
+        url: "https://api.darksky.net/forecast/2ba7132f09fc2069522d9a980d4b741b/42.4851,-71.4328",
+        //data: myData,
+    	type: 'GET',
+    	crossDomain: true,
+	    dataType: 'jsonp'
+	    
     }).then(function(data) {
        $('.greeting-id').append(data.currently.summary);
        $('.greeting-content').append(data.content);
