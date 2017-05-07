dbe01ab7201eba0b59ba9550de6ec622fc0e2298
Jonathan Cohn <shred444@gmail.com>
2017-05-07 19:52:46 -0400

    dd

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.ajax({
         url: "https://api.darksky.net/forecast/2ba7132f09fc2069522d9a980d4b741b/42.4851,-71.4328"
     }).then(function(data) {
-       $('.greeting-id').append(data.currently);
+       $('.greeting-id').append(data.currently.summary);
        $('.greeting-content').append(data.content);
     });
 });
