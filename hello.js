f47ffb533eb46052b31cb9064c88c0174144571f
Jonathan Cohn <shred444@gmail.com>
2017-05-07 20:02:04 -0400

    linked to particle

--- hello.js
+++ hello.js
@@ -7,7 +7,21 @@ $(document).ready(function() {
 	    dataType: 'jsonp'
 	    
     }).then(function(data) {
-       $('.greeting-id').append(data.currently.summary);
-       $('.greeting-content').append(data.content);
+       $('.current-weather').append(data.currently.summary);
+       //$('.greeting-content').append(data.content);
     });
+    
+    
+    $.ajax({
+        url: "api.particle.io/v1/devices/26001c001247353136383631/air?access_token=e5ad56d7b6af5befcbf3992275e508b1c2911996",
+        //data: myData,
+    	type: 'GET',
+    	crossDomain: true,
+	    dataType: 'jsonp'
+	    
+    }).then(function(data) {
+       $('.greenhouse-temp').append(data.result);
+       //$('.greeting-content').append(data.content);
+    });
+    
 });
